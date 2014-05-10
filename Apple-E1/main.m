4de246da45023c7b4bf379200f1311ddf622f6fe
ANDRES KWAN <andreskwan@gmail.com>
2014-05-10 02:41:03 -0500

    Initial Commit

---
+++ Apple-E1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Apple-E1
+//
+//  Created by Andres Kwan on 5/10/14.
+//  Copyright (c) 2014 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KCAppDelegate class]));
+    }
+}
---
+++ Apple-E1Tests/Apple_E1Tests.m
@@ -0,0 +1,34 @@
+//
+//  Apple_E1Tests.m
+//  Apple-E1Tests
+//
+//  Created by Andres Kwan on 5/10/14.
+//  Copyright (c) 2014 Kwan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Apple_E1Tests : XCTestCase
+
+@end
+
+@implementation Apple_E1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
