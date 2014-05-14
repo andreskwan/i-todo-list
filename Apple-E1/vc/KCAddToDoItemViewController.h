d7e04429cc107884f19560781735de371df0737b
ANDRES KWAN <andreskwan@gmail.com>
2014-05-14 16:54:36 -0500

    Custom VC for View and TableView

---
+++ Apple-E1/vc/KCAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  KCAddToDoItemViewController.h
+//  Apple-E1
+//
+//  Created by Andres Kwan on 5/14/14.
+//  Copyright (c) 2014 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KCAddToDoItemViewController : UIViewController
+
+@end
---
+++ Apple-E1/vc/KCToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KCToDoListTableViewController.h
+//  Apple-E1
+//
+//  Created by Andres Kwan on 5/14/14.
+//  Copyright (c) 2014 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KCToDoListTableViewController : UITableViewController
+
+@end
